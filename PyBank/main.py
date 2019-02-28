68dade84615d8bc11ab496be036d2456b8506b28
singtoloco <vanich.vacharussiriyuth@gmail.com>
2019-02-28 15:55:52 -0600

    3rd commit; redo HW3 without pandas

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,56 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 21 10:17:45 2019
-
 @author: vvacharussiriyuth
 """
-
-import pandas as pd
-import numpy as np
 import os
-
-#This is temp path, will have to figure out later
-#csv_path = "C:/Users/vvacharussiriyuth/OneDrive - Equitec Group, LLC/LNVshare/LNV BTS/csv/csv others/budget_data.csv"
-#csv_path = "C:/Users/singt/NUCHI201902DATA1/Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv"
+import csv
 
 #This method returns current working directory of a process.
 print(os.getcwd())
 
-#make sure we, on GitBash, cd to the parent folder of Resources before using ..
-#Relative reference for filepath
-#We can use os.path.join
-#csv_path = os.path.join('..', 'Resources', 'budget_data.csv')
 csv_path = "budget_data.csv"
-#OR
-#csv_path = "../Resources/budget_data.csv"
-###The ".." means from current directory, go up one folder.
 
-
-df_budget = pd.read_csv(csv_path)
+with open(csv_path, newline='') as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+    date = []
+    pnl = []
+    
+    for row in csvreader:
+        date.append(row[0])
+        pnl.append(row[1])
 
 #The total number of months included in the dataset
-total_months = len(df_budget['Date'])
+total_months = len(date)
 
+pnl = [int(i) for i in pnl]
 #The net total amount of "Profit/Losses" over the entire period
-total_pnl = sum(df_budget['Profit/Losses'])
+total_pnl = sum(pnl)
 
 chg = []
 
+#for i in range(total_months-1):
 for i in range(total_months-1):
-    chg.append(df_budget.iloc[i+1][1] - df_budget.iloc[i][1])
+    chg.append(pnl[i+1] - pnl[i])
     
 #The average of the changes in "Profit/Losses" over the entire period
-avg_chg = np.average(chg)
+avg_chg = sum(chg)/(total_months-1)
 
 #The greatest increase in profits (date and amount) over the entire period
 max_chg = max(chg)
 i_max_chg = chg.index(max_chg)
-month_max_chg = df_budget.iloc[i_max_chg+1][0]
+month_max_chg = date[i_max_chg + 1]
 
 #The greatest decrease in losses (date and amount) over the entire period
 min_chg = min(chg)
 i_min_chg = chg.index(min_chg)
-month_min_chg = df_budget.iloc[i_min_chg+1][0]
+month_min_chg = date[i_min_chg + 1]
+
 
 #Print results to terminal
 str_results = ("Financial Analysis" '\n' "----------------------------" '\n'\
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,86 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 22 09:05:16 2019
+
 
 @author: vvacharussiriyuth
 """
-
-import pandas as pd
-#import numpy as np
 import os
-
-#This is temp path, will have to figure out later
-#csv_path = "C:/Users/vvacharussiriyuth/OneDrive - Equitec Group, LLC/LNVshare/LNV BTS/csv/csv others/election_data_sample.csv"
-#csv_path = "C:/Users/singt/NUCHI201902DATA1/Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv"
+import csv
 
 #This method returns current working directory of a process.
 print(os.getcwd())
 
-#make sure we, on GitBash, cd to the parent folder of Resources before using ..
-#Relative reference for filepath
-#We can use os.path.join
-#csv_path = os.path.join('..', 'Resources', 'election_data.csv')
 csv_path = "election_data.csv"
-#OR
-#csv_path = "../Resources/election_data.csv"
-###The ".." means from current directory, go up one folder.
 
-df_poll = pd.read_csv(csv_path)
-
-##Testing duplicated by adding fake dup IDs
-#df2 = df_poll.iloc[0:7][:]
-#df_poll = df_poll.append(df2)
-
-df_id_dup = df_poll[df_poll['Voter ID'].duplicated(keep=False)]
-
-if df_id_dup.empty:
-    print("Good, there are NO duplicated Voter IDs" '\n')
-else:
-    print("Warning!!!, there are duplicated Voter IDs" '\n')
+with open(csv_path, newline='') as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+    voter = []
+    county = []
+    candidate = []
+    
+    for row in csvreader:
+        voter.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
 
 #The total number of votes cast
-total_votes = len(df_poll['Voter ID'])
+total_votes = len(voter)
 
 #A complete list of candidates who received votes
-#ref: https://stackoverflow.com/questions/32072076/find-the-unique-values-in-a-column-and-then-sort-them
-""".unique() gives object??? and .drop_duplicate() is faster"""
-##using .unique()
-#list_candidates = df_poll.Candidate.unique() 
+candidate_unique = []
+for x in candidate:
+    if x not in candidate_unique:
+        candidate_unique.append(x)    
 
-##using .drop_duplicate()
-cdds = df_poll.Candidate.drop_duplicates()
-##reset index for easier looping
-cdds.reset_index(drop=True, inplace=True)
-#print(type(cdds))
-#print(cdds)
-
-"""Counting votes of each candidate can be done in multiple ways as the followings"""
-#len(df_poll[df_poll.Candidate == 'Khan'])
-#df_poll[df_poll["Candidate"] == 'Khan'].count()["Candidate"]
-
+#The total number of votes each candidate won
 v_count = []
+#The percentage of votes each candidate won
 v_pct = []
 str1 = ""
 
-for i in range(len(cdds)):
-    v_count.append(len(df_poll[df_poll.Candidate == cdds[i]]))
-    v_pct.append(v_count[i]/total_votes)
-    str1 += (f"{cdds[i]}: " + "{:.3%}".format(v_pct[i]) + f" ({v_count[i]})" '\n')
+for y in candidate_unique:
+    v_count.append(candidate.count(y))
 
-#print(str1)
-    
-#Checking v
-#if sum(v_count) !=  total_votes or sum(v_pct) != 1:
-#    print("something is wrong!!!")
+v_pct = [z/total_votes for z in v_count]
+
+for i in range(len(v_count)):
+    str1 += (f"{candidate_unique[i]}: " + "{:.3%}".format(v_pct[i]) + f" ({v_count[i]})" '\n')
     
 #The winner of the election based on popular vote.
 max_votes = max(v_count)
 i_max_votes = v_count.index(max_votes)
-winner = cdds[i_max_votes]
-
-#print ("{:.3%}".format(v_pct[0]))
-
-
+winner = candidate_unique[i_max_votes]
 
 str_results = ("Election Results" '\n' "-------------------------" '\n'\
       f"Total Votes: {total_votes}" '\n' "-------------------------" '\n')
